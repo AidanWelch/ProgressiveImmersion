c69aed81aaa71c5d381650572e26ef1071cf3709
Aidan Welch <aidansw@yahoo.com>
2022-04-27 09:07:36 -0400

    update language-select.js to js standards

--- popup/language-select.js
+++ popup/language-select.js
@@ -8,13 +8,13 @@ const xhr = new XMLHttpRequest();
 xhr.open("GET", "https://ssl.gstatic.com/inputtools/js/ln/17/en.js", true);
 xhr.onreadystatechange = () => {
     if(xhr.readyState === 4){
-        var nativeNames = xhr.responseText.slice(30);
+        let nativeNames = xhr.responseText.slice(30);
         nativeNames = nativeNames.slice(nativeNames.indexOf("{"), nativeNames.indexOf("}") + 1);
         nativeNames = nativeNames.replaceAll("'", '"')
         nativeNames = JSON.parse(nativeNames);
-        for (var isoCode in nativeNames) {
-            var listItem = document.createElement("li");
-            var button = document.createElement("button");
+        for (let isoCode in nativeNames) {
+            const listItem = document.createElement("li");
+            const button = document.createElement("button");
             button.classList.add("w3-button");
             button.innerHTML = nativeNames[isoCode];
             button.isoCode = isoCode;
