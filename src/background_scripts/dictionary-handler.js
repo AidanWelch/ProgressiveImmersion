a3af1427e51d0fd35102c57370b8b4bb3ed5d9a8
Aidan Welch <aidan@freedwave.com>
2022-05-03 16:18:22 -0400

    define updateDictionary() function

--- src/background_scripts/dictionary-handler.js
+++ src/background_scripts/dictionary-handler.js
@@ -2,13 +2,34 @@ import browser from "webextension-polyfill";
 import 'setimmediate'
 import translate from '@vitalets/google-translate-api'
 
-(async function () {
-	const res = await translate('Ik spreek Engels', {to: 'en'});
-	console.log(res.text);
-})();
-
 function updateDictionary(){
-	browser.storage.local.get([ "wordpack", "wordpackIndex", "wordQueue"])
+	browser.storage.local.get([ "wordpack", "wordpackIndex", "wordQueue", "origin", "target", "dictionary"]).then( value => {
+		
+		if (value.origin === undefined || value.target === undefined || value.wordQueue === undefined || value.wordQueue.length === 0) {
+			return;
+		}
+
+		if (value.dictionary === undefined) {
+			value.dictionary = {};
+		}
+		if (value.dictionary[value.origin] === undefined) {
+			value.dictionary[value.origin] = {};
+		}
+		if (value.dictionary[value.origin][value.target] === undefined) {
+			value.dictionary[value.origin][value.target] = {};
+		}
+
+		const word = value.wordQueue.sort((a, b) => a[1] - b[1]).pop();
+
+		translate(word[0], {from: value.origin, to: value.target}).then( res => {
+			value.dictionary[value.origin][value.target][word[0]] = res.text;
+			browser.storage.local.set({
+				dictionary: value.dictionary,
+				wordQueue: value.wordQueue
+			});
+		});
+
+	});
 }
 
 export default updateDictionary
