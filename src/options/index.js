fa1b0f4ea6b8234897f55ea358a5e38e0883b1ed
Aidan Welch <aidan@freedwave.com>
2022-05-05 22:40:29 -0400

    add reset buttons to advanced options

--- src/options/index.js
+++ src/options/index.js
@@ -11,6 +11,11 @@ document.getElementById("updateFrequency").oninput = () => {
 	browser.storage.local.set({ updateFrequency: updateFrequency });
 };
 
+document.getElementById("updateFrequencyReset").onclick = () => {
+	document.getElementById("updateFrequency").value = DEFAULT_UPDATE_FREQUENCY;
+	browser.storage.local.set({ updateFrequency: DEFAULT_UPDATE_FREQUENCY });
+};
+
 // Min Word Length Option
 browser.storage.local.get("minWordLength").then((value) => {
 	document.getElementById("minWordLength").value = (value.minWordLength !== undefined) ? value.minWordLength : DEFAULT_MIN_WORD_LENGTH;
@@ -22,6 +27,11 @@ browser.storage.local.get("minWordLength").then((value) => {
 document.getElementById("minWordLength").oninput = () => {
 	const minWordLength = parseFloat(document.getElementById("minWordLength").value);
 	browser.storage.local.set({ minWordLength: minWordLength });
+};
+
+document.getElementById("minWordLengthReset").onclick = () => {
+	document.getElementById("minWordLength").value = DEFAULT_MIN_WORD_LENGTH;
+	browser.storage.local.set({ minWordLength: DEFAULT_MIN_WORD_LENGTH });
 };
 
 // Words to Save Option
@@ -37,6 +47,11 @@ document.getElementById("wordsToSave").oninput = () => {
 	browser.storage.local.set({ wordsToSave: wordsToSave });
 };
 
+document.getElementById("wordsToSaveReset").onclick = () => {
+	document.getElementById("wordsToSave").value = DEFAULT_WORDS_TO_SAVE;
+	browser.storage.local.set({ wordsToSave: DEFAULT_WORDS_TO_SAVE });
+};
+
 // Filter Max Share of Words Option
 browser.storage.local.get("filterMaxShareOfWords").then((value) => {
 	document.getElementById("filterMaxShareOfWords").value = (value.filterMaxShareOfWords !== undefined) ? value.filterMaxShareOfWords : DEFAULT_FILTER_MAX_SHARE_OF_WORDS;
@@ -50,6 +65,11 @@ document.getElementById("filterMaxShareOfWords").oninput = () => {
 	browser.storage.local.set({ filterMaxShareOfWords: filterMaxShareOfWords });
 };
 
+document.getElementById("filterMaxShareOfWordsReset").onclick = () => {
+	document.getElementById("filterMaxShareOfWords").value = DEFAULT_FILTER_MAX_SHARE_OF_WORDS;
+	browser.storage.local.set({ filterMaxShareOfWords: DEFAULT_FILTER_MAX_SHARE_OF_WORDS });
+};
+
 // Filter Min Share of Words Option
 browser.storage.local.get("filterMinShareOfWords").then((value) => {
 	document.getElementById("filterMinShareOfWords").value = (value.filterMinShareOfWords !== undefined) ? value.filterMinShareOfWords : DEFAULT_FILTER_MIN_SHARE_OF_WORDS;
@@ -61,4 +81,9 @@ browser.storage.local.get("filterMinShareOfWords").then((value) => {
 document.getElementById("filterMinShareOfWords").oninput = () => {
 	const filterMinShareOfWords = parseFloat(document.getElementById("filterMinShareOfWords").value);
 	browser.storage.local.set({ filterMinShareOfWords: filterMinShareOfWords });
+};
+
+document.getElementById("filterMinShareOfWordsReset").onclick = () => {
+	document.getElementById("filterMinShareOfWords").value = DEFAULT_FILTER_MIN_SHARE_OF_WORDS;
+	browser.storage.local.set({ filterMinShareOfWords: DEFAULT_FILTER_MIN_SHARE_OF_WORDS });
 };
