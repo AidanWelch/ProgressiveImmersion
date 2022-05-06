c9e76324ef58068f707e007805cfe6382934b161
Aidan Welch <aidan@freedwave.com>
2022-05-05 23:13:13 -0400

    implement basic translate to pages

--- src/content_scripts/translate.js
+++ src/content_scripts/translate.js
@@ -1,3 +1,14 @@
-progressiveImmersion.translate = function (elem) {
-	elem.textContent = "test";
+progressiveImmersion.translate = function (elem, dictionary, origin, target) {
+
+	if (origin === undefined || target === undefined || dictionary === undefined || dictionary[origin] === undefined || typeof dictionary[origin][target] !== "object") {
+		return;
+	}
+
+	const dictionaryPage = dictionary[origin][target];
+
+	for (let word in dictionaryPage) {
+		const regex = new RegExp(`\\b${word}\\b`, "gu");
+		elem.textContent = elem.textContent.replaceAll(regex, dictionaryPage[word]);
+	}
+
 }
