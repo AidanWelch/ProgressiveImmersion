ebe4f38fda8d585c1cc56bdbf1116608ffd247d7
Aidan Welch <aidan@freedwave.com>
2022-12-25 23:44:35 +0100

    Cookie remove removed since omit in translate 10.5

--- src/background_scripts/dictionary-handler.js
+++ src/background_scripts/dictionary-handler.js
@@ -28,53 +28,13 @@ function updateDictionary(){
 			}
 		}
 
-		let attempts = 0;
-
-		function doTranslation() {
-			attempts++;
-			browser.cookies.getAll({domain: ".google.com"}).then( cookies => {
-				const promises = [];
-				for (let cookie of cookies) {
-					promises.push(browser.cookies.remove({url: "https://" + cookie.domain + cookie.path, name: cookie.name}).then( res => {return (res !== null) ? cookie : false} ));
-				}
-				return Promise.all(promises);
-			}).then( (cookies) => {
-				return translate(word[0], {from: value.origin, to: value.target}).then( res => {
-					value.dictionary[value.origin][value.target][word[0]] = res.text;
-					browser.storage.local.set({
-						dictionary: value.dictionary,
-						wordQueue: value.wordQueue
-					});
-				}).then( () => {
-					for (let cookie of cookies) {
-						if (cookie) {
-							const new_cookie = {
-								//domain: cookie.domain,
-								expirationDate: cookie.expirationDate,
-								httpOnly: cookie.httpOnly,
-								name: cookie.name,
-								path: cookie.path,
-								sameSite: cookie.sameSite,
-								secure: cookie.secure,
-								//storeId: cookie.storeId,
-								url: "https://" + cookie.domain + cookie.path,
-								value: cookie.value
-							}
-							browser.cookies.set(new_cookie);
-						}
-					}
-				});
-			}).catch((err) => {
-				if (attempts === 2) {
-					console.error(err);
-				} else {
-					console.warn(err);
-					doTranslation();
-				}
+		translate(word[0], {from: value.origin, to: value.target}).then( res => {
+			value.dictionary[value.origin][value.target][word[0]] = res.text;
+			browser.storage.local.set({
+				dictionary: value.dictionary,
+				wordQueue: value.wordQueue
 			});
-		}
-
-		doTranslation();
+		});
 	});
 }
 
