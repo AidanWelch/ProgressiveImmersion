710ac22ed14e1cbbe435c731f0864754fff386eb
Aidan Welch <aidan@freedwave.com>
2023-06-05 12:23:05 +0200

    Stop looping through HTML so much

--- src/content_scripts/main.js
+++ src/content_scripts/main.js
@@ -30,7 +30,7 @@ browser.storage.local.get( [ 'state', 'dictionary', 'origin', 'target', 'minWord
 		origin = value.origin;
 		target = value.target;
 		minWordLength = value.minWordLength ?? minWordLength;
-		const matchTags = [ 'p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'li', 'th', 'td', 'span', 'a' ];
+		const matchTags = [ 'p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'li', 'th', 'td', 'a' ];
 		matchTags.forEach( ( tag ) => {
 			const elems = document.body.getElementsByTagName( tag );
 			for ( let i = 0; i < elems.length; i++ ){
@@ -42,20 +42,83 @@ browser.storage.local.get( [ 'state', 'dictionary', 'origin', 'target', 'minWord
 	}
 });
 
+const matchLetter = /\p{L}/u;
 const viewObserver = new IntersectionObserver( ( entries ) => {
 	entries.forEach( entry => {
 		if ( entry.isIntersecting && !entry.target.analyzed ){
 			entry.target.analyzed = true;
-			const words = entry.target.innerText.match( /[\p{L}-]+/ug );
-			if ( words ){
-				for ( const word of words ){
-					if ( word.length >= minWordLength ){
-						countWord( word.toLowerCase() );
+			const innerHTML = entry.target.innerHTML;
+			let result = '';
+			let inTag = false;
+			let inTagString = false;
+			let inWord = false;
+			// let inUrl = false; // to prevent for example https://freedwave.com/test/ from matching "freedwave", "com", or "test" TODO - also avoid filepaths
+			let inEscapedSymbol = false;
+			let wordStart = 0;
+			for ( let i = 0; i < innerHTML.length; i++ ) {
+				if ( inTag ) {
+					if ( innerHTML[i] === '"' ) {
+						inTagString = !inTagString;
+					} else if ( innerHTML[i] === '>' && !inTagString ) {
+						inTag = false;
 					}
+
+					result += innerHTML[i];
+					continue;
 				}
+
+				if ( matchLetter.test( innerHTML[i] ) & !inEscapedSymbol ) {
+					if ( !inWord ) {
+						inWord = true;
+						wordStart = i;
+					}
+
+					continue;
+				} else if ( inWord ) {
+					const word = innerHTML.slice( wordStart, i );
+					const wordLower = word.toLowerCase();
+					if ( word.length >= minWordLength ) {
+						countWord( wordLower );
+					}
+
+					result += translate( wordLower, word, dictionary, origin, target );
+					inWord = false;
+				} else {
+					inEscapedSymbol = false;
+				}
+
+				if ( innerHTML[i] === '<' ) {
+					inTag = true;
+				}
+
+				if ( innerHTML[i] === '&' ) {
+					inEscapedSymbol = true;
+				}
+
+				result += innerHTML[i];
 			}
 
-			translate( entry.target, dictionary, origin, target );
+			if ( inWord ) {
+				const word = innerHTML.slice( wordStart, innerHTML.length );
+				const wordLower = word.toLowerCase();
+				if ( word.length >= minWordLength ) {
+					countWord( wordLower );
+				}
+
+				result += translate( wordLower, word, dictionary, origin, target );
+			}
+
+			entry.target.innerHTML = result;
+
+			const wordElements = entry.target.getElementsByTagName( 'progressive-immersion-word' );
+			for ( const word of wordElements ) {
+				word.addEventListener( 'mouseover', e => {
+					e.target.textContent = e.target.getAttribute( 'data_original' );
+				});
+				word.addEventListener( 'mouseout', e => {
+					e.target.textContent = e.target.getAttribute( 'data_translated' );
+				});
+			}
 		}
 	});
 });
--- src/content_scripts/translate.js
+++ src/content_scripts/translate.js
@@ -1,28 +1,19 @@
-function translate ( elem, dictionary, origin, target ) {
+function translate ( wordLower, originalWord, dictionary, origin, target ) {
 	if ( dictionary?.[origin]?.[target] === undefined ) {
-		return;
+		return originalWord;
 	}
 
 	const dictionaryPage = dictionary[origin][target];
+	if ( Object.prototype.hasOwnProperty.call( dictionaryPage, wordLower ) ) {
+		let translated = dictionaryPage[wordLower];
+		if ( wordLower !== originalWord ) {
+			translated = translated.charAt( 0 ).toUpperCase() + translated.slice( 1 );
+		}
 
-	const regex = new RegExp( '(?<!<[^<>]*)\\b(' +
-		Object.keys( dictionaryPage ).join( '|' )
-		+ ')\\b(?![^<>]*>)', 'gui' );
-
-	elem.innerHTML = elem.innerHTML.replaceAll( regex, ( match ) => {
-		const word = match.toLowerCase();
-		return `<progressive-immersion-word data_original="${match}" data_translated="${dictionaryPage[word]}">${dictionaryPage[word]}</progressive-immersion-word>`;
-	});
-
-	const wordElements = elem.getElementsByTagName( 'progressive-immersion-word' );
-	for ( const word of wordElements ) {
-		word.addEventListener( 'mouseover', e => {
-			e.target.textContent = e.target.getAttribute( 'data_original' );
-		});
-		word.addEventListener( 'mouseout', e => {
-			e.target.textContent = e.target.getAttribute( 'data_translated' );
-		});
+		return `<progressive-immersion-word data_original="${originalWord}" data_translated="${translated}">${translated}</progressive-immersion-word>`;
 	}
+
+	return originalWord;
 }
 
 export default translate;
