fc1e8b273a51e6eab4c4de8f8bcf027ebb97a546
Aidan Welch <aidan@freedwave.com>
2023-12-25 05:44:11 +0100

    attempt to fix dynamically loaded sites

--- src/content_scripts/main.js
+++ src/content_scripts/main.js
@@ -5,10 +5,42 @@ import {
 import countWord from './analyze';
 import translate from './translate';
 
+function capitalizationPermutations ( stringArray ){
+	const result = [];
+	for ( const s of stringArray ) {
+		result.push( s.toUpperCase() );
+		result.push( s.toLowerCase() );
+	}
+
+	return result;
+}
+
+const TAGS_TO_TRANSLATE = capitalizationPermutations( [ 'p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'li', 'th', 'td', 'a', 'i', 'em', 'strong', 'mark', 'ul', 'main' ] );
+const TAGS_TO_TRANSLATE_WHEN_NESTED_IN_TRACKED_TAGS = capitalizationPermutations( [ 'div', 'span' ] ); // thanks tagName for being inconsistent!
+
 let dictionary = undefined;
 let origin = undefined;
 let target = undefined;
 let minWordLength = DEFAULT_MIN_WORD_LENGTH;
+
+function checkAncestorInTags ( node, tags ) {
+	if ( !Array.isArray( tags ) ) {
+		tags = [ tags ];
+	}
+
+	if ( node.parentNode && tags.includes( node.parentNode.tagName ) ) {
+		return true;
+	}
+
+	return node.parentNode !== null ? checkAncestorInTags( node.parentNode, tags ) : false;
+}
+
+function checkShouldTranslateNode ( node ) {
+	return TAGS_TO_TRANSLATE.includes( node.tagName ) || (
+		TAGS_TO_TRANSLATE_WHEN_NESTED_IN_TRACKED_TAGS.includes( node.tagName ) &&
+		checkAncestorInTags( node, TAGS_TO_TRANSLATE )
+	);
+}
 
 browser.storage.local.get( [ 'state', 'dictionary', 'origin', 'target', 'minWordLength', 'exclusionList', 'exclusionListMode' ] ).then( value => {
 	let isExcluded = false;
@@ -25,27 +57,86 @@ browser.storage.local.get( [ 'state', 'dictionary', 'origin', 'target', 'minWord
 		});
 	}
 
+	// this method of element selection is currently leading to a lot double(or more) tallying of words
+	// but it is at least in some form needed for dynamically rendered sites(like gmail)
+	// the double counting doesn't seem to be harming word selection for now but if it gets bad
+	// then probably the solution will be just analyzing text nodes
+	const mutationObserver = new MutationObserver( function ( mutationRecords ) {
+		for ( const mutation of mutationRecords ) {
+			if ( ( mutation.type === 'characterData' || mutation.type === 'attributes' ) && mutation.target.innerText && checkShouldTranslateNode( mutation.target ) ) {
+				viewObserver.observe( mutation.target ); // make sure it is being observed
+				return;
+			}
+
+			if ( !mutation.addedNodes ) {
+				return;
+			}
+
+			for ( const addedNode of mutation.addedNodes ) {
+				( function observeNodesAndChildren ( node ){
+					if (
+						checkShouldTranslateNode( node )
+					) {
+						if ( node.innerText ) {
+							viewObserver.observe( node );
+						}
+
+						mutationObserver.observe( node, { characterData: true, attributes: true });
+					}
+
+					for ( const child of node.childNodes ) {
+						observeNodesAndChildren( child );
+					}
+				})( addedNode );
+			}
+		}
+	});
+
+	mutationObserver.observe( document.body, {
+		subtree: true,
+		childList: true
+	});
+
 	if ( value.state && !isExcluded ){
 		dictionary = value.dictionary;
 		origin = value.origin;
 		target = value.target;
 		minWordLength = value.minWordLength ?? minWordLength;
-		const matchTags = [ 'p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'li', 'th', 'td', 'a' ];
-		matchTags.forEach( ( tag ) => {
-			const elems = document.body.getElementsByTagName( tag );
-			for ( let i = 0; i < elems.length; i++ ){
-				if ( elems.item( i ).textContent ){
-					viewObserver.observe( elems.item( i ) );
-				}
+
+		const elems = document.body.querySelectorAll( TAGS_TO_TRANSLATE.join( ',' ) );
+		for ( const elem of elems ){
+			// elem.style.backgroundColor = "#0A0A0A" // For debugging
+			if ( elem.innerText ){
+				viewObserver.observe( elem );
 			}
-		});
+
+			mutationObserver.observe( elem, { characterData: true, attributes: true });
+		}
+
+		const nestedElems = document.body.querySelectorAll( TAGS_TO_TRANSLATE_WHEN_NESTED_IN_TRACKED_TAGS.join( ',' ) );
+		for ( const nestedElem of nestedElems ) {
+			if ( checkAncestorInTags( nestedElem, TAGS_TO_TRANSLATE ) ){
+				// nestedElem.style.backgroundColor = "#A00000" // For debugging
+				if ( nestedElem.innerText ) {
+					viewObserver.observe( nestedElem );
+				}
+
+				mutationObserver.observe( nestedElem, { characterData: true, attributes: true });
+			}
+		}
 	}
 });
 
 const matchLetter = /\p{L}/u;
 const viewObserver = new IntersectionObserver( ( entries ) => {
 	entries.forEach( entry => {
+		/* entry.target.style.backgroundColor = "#AA0000"; // For debugging
+		if ( entry.isIntersecting && entry.target.analyzed) {
+			entry.target.style.backgroundColor = "#00AA00";
+		} */
+
 		if ( entry.isIntersecting && !entry.target.analyzed ){
+			// entry.target.style.backgroundColor = "#0000AA"; // For debugging
 			entry.target.analyzed = true;
 			const innerHTML = entry.target.innerHTML;
 			let result = '';
